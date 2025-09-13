6bafd3af1d4a91d652463b103f2e382cd179e887
Vyacheslav <slawa.121@yandex.ru>
2025-09-13 15:19:28 +0300

    Решение задачи вынесенно в отдельный класс

--- hanoi_tower.py
+++ hanoi_tower.py
@@ -1,6 +1,5 @@
 from rod import Rod
 
-
 class HanoiTower:
     """Класс, реализующий игру Ханойские башни."""
     def __init__(self, num_disks):
@@ -22,27 +21,18 @@ class HanoiTower:
         if disk is not None:
             self.rods[destination].push(disk)
             self.move_count += 1
-            print(f"Шаг {self.move_count}: Переместить диск {disk} с {source} на {destination}")
+            self.print_move(disk, source, destination)
             self.print_state()
+
+    def print_move(self, disk, source, destination):
+        """Вывести шаг перемещения диска."""
+        print(f"Шаг {self.move_count}: Переместить диск {disk} с {source} на {destination}")
 
     def print_state(self):
         """Вывести текущее состояние стержней."""
         for rod in self.rods.values():
             print(rod)
         print("-" * 30)
-
-    def solve(self, n=None, source='A', auxiliary='B', destination='C'):
-        """Рекурсивно решить задачу Ханойских башен."""
-        if n is None:
-            n = self.num_disks
-
-        if n > 0:
-            # Переместить n-1 дисков с source на auxiliary через destination
-            self.solve(n - 1, source, destination, auxiliary)
-            # Переместить n-й диск с source на destination
-            self.move_disk(source, destination)
-            # Переместить n-1 дисков с auxiliary на destination через source
-            self.solve(n - 1, auxiliary, source, destination)
 
     def get_move_count(self):
         """Вернуть общее количество шагов."""
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from hanoi_tower import HanoiTower
-from hanoi_tower_heuristic import HanoiTowerHeuristic
-
+from hanoi_solver import HanoiSolver
 
 def main():
     try:
@@ -8,19 +7,20 @@ def main():
         if num_disks <= 0:
             raise ValueError("Количество дисков должно быть положительным.")
 
-        # game = HanoiTower(num_disks)
-        game = HanoiTowerHeuristic(num_disks)
+        game = HanoiTower(num_disks)
         print("Начальное состояние:")
         game.print_state()
+
+        solver = HanoiSolver()
         print("Решение:")
-        game.solve()
+        moves = solver.get_solution(game)
         print(f"Решение завершено. Всего шагов: {game.get_move_count()}")
+        # print("Список шагов:", moves)
 
     except ValueError as e:
         print(f"Ошибка: {e}")
     except Exception as e:
         print(f"Произошла ошибка: {e}")
-
 
 if __name__ == "__main__":
     main()
